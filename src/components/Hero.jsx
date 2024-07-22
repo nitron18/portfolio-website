27f579c370f3e338d552cecfd39ca65929f75df5
nitron18 <ananthchaitanya17@gmail.com>
2024-07-22 22:56:47 +0530

    Added about and technologies section. Technologies sections needs edits later.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,7 +18,7 @@ const Hero = () => {
     </div>
     <div className="w-full lg:w-1/2 lg:p-8">
      <div className="flex justify-center">
-        <img src={profilePic} alt="Ananth Chaitanya" />
+        <img className="rounded-2xl" src={profilePic} alt="Ananth Chaitanya" />
      </div>
     </div>
    </div>
