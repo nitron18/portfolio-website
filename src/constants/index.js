7b44b39187a2514273f1b817d95a1080fb6f8b3f
nitron18 <ananthchaitanya17@gmail.com>
2024-07-25 22:06:48 +0530

    Added contacts page. Also added motions to objects using framer motion.

--- src/constants/index.js
+++ src/constants/index.js
@@ -70,7 +70,7 @@ export const PROJECTS = [
 ];
 
 export const CONTACT = {
-  address: "767 Fifth Avenue, New York, NY 10153 ",
-  phoneNo: "+12 4555 666 00 ",
-  email: "me@example.com",
+  address: "3-5-929, Susheelwadi, Himayat nagar, Hyderabad-500029",
+  phoneNo: "+91 7680805131",
+  email: "ananthchaitanya17@gmail.com",
 };
